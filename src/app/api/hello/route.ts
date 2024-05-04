c004e41318b7e5dc62f229bf11ec474b29fe75da
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2024-05-04 15:32:10 +0200

    Added biome

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,30 +1,21 @@
-import type { NextRequest } from "next/server";
-import { getRequestContext } from "@cloudflare/next-on-pages";
+import type { NextRequest } from 'next/server'
+import { getRequestContext } from '@cloudflare/next-on-pages'
 
-export const runtime = "edge";
+export const runtime = 'edge'
 
 export async function GET(request: NextRequest) {
-  let responseText = "Hello World";
+	const db = getRequestContext().env.DB
 
-  // In the edge runtime you can use Bindings that are available in your application
-  // (for more details see:
-  //    - https://developers.cloudflare.com/pages/framework-guides/deploy-a-nextjs-site/#use-bindings-in-your-nextjs-application
-  //    - https://developers.cloudflare.com/pages/functions/bindings/
-  // )
-  //
-  // KV Example:
-  // const myKv = getRequestContext().env.MY_KV_NAMESPACE
-  // await myKv.put('suffix', ' from a KV store!')
-  // const suffix = await myKv.get('suffix')
-  // responseText += suffix
+	const { results } = await db
+		.prepare('SELECT * FROM Persons WHERE Name LIKE ?')
+		.bind(request.nextUrl.searchParams.get('name') || '%')
+		.all()
 
-  // this is the KV binding you defined in the wrangler.toml file
-  const db = getRequestContext().env.DB;
-
-  const { results } = await db
-    .prepare("SELECT * FROM Persons WHERE Name LIKE ?")
-    .bind(request.nextUrl.searchParams.get("name") || "%")
-    .all();
-
-  return Response.json(results);
+	return Response.json(results)
 }
+
+// In the edge runtime you can use Bindings that are available in your application
+// (for more details see:
+//    - https://developers.cloudflare.com/pages/framework-guides/deploy-a-nextjs-site/#use-bindings-in-your-nextjs-application
+//    - https://developers.cloudflare.com/pages/functions/bindings/
+// )
