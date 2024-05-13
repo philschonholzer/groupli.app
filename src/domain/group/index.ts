79f12dc4353a60f168718502379af32037b5845b
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2024-05-13 17:36:23 +0200

    chore: Refactor group creation logic and update imports

--- src/domain/group/index.ts
+++ src/domain/group/index.ts
@@ -1,15 +1,14 @@
 import { Effect } from 'effect'
 import { nanoid } from 'nanoid'
-import { Repository } from './repository'
 import { Round } from '..'
+import { Repository } from './repository'
 
 export * from './repository'
 
 export const newGroup = Effect.gen(function* () {
-	const repository = yield* Repository
 	const id = nanoid(8)
 	const name = 'New Group'
-	yield* repository.insert({ id, name })
+	yield* Repository.insert({ id, name })
 	yield* Round.newRound(id, [])
 	return { id, name }
 })
