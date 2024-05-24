0049d8c62c9c47731c4bb721702620f9820b8c71
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2024-05-24 17:45:01 +0200

    Hide existing groups

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import { run } from '@/adapter/effect'
 import { Group } from '@/domain'
 import { getRequestContext } from '@cloudflare/next-on-pages'
 import { Effect } from 'effect'
-import Link from 'next/link'
 import { newGroup } from './action'
 
 export const runtime = 'edge'
@@ -23,15 +22,6 @@ export default async function Home() {
 				>
 					<button type="submit">Create New Group</button>
 				</form>
-
-				<h2>Existing groups</h2>
-				{groups.map((group) => (
-					<div key={group.id}>
-						<Link href={`/group/${group.id}`}>
-							<h1>{group.name}</h1>
-						</Link>
-					</div>
-				))}
 			</div>
 		)
 	}).pipe(
