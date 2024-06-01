1f9acb658aa89a2cabe5416e0b20931ec3c3070b
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2024-06-01 18:40:17 +0200

    Fixed build and added studio 🎉

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'drizzle-kit'
 export default defineConfig({
 	schema: './src/adapter/db/schema.ts',
-	driver: 'd1',
+	driver: 'd1-http',
 	verbose: true,
 	strict: true,
 	out: './drizzle',
+	dialect: 'sqlite',
+	dbCredentials: {
+		accountId: process.env.DRIZZLE_CF_ACCOUNT_ID,
+		databaseId: process.env.DRIZZEL_DATABASE_ID,
+		token: process.env.DRIZZLE_D1_TOKEN,
+	},
 })
