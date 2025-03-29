a4dbd15b412b83eb4d4f06b5fee69fa8f143d80b
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2025-03-29 15:37:23 +0100

    Close db on shutdown

--- src/adapter/db/orm-client.ts
+++ src/adapter/db/orm-client.ts
@@ -5,12 +5,17 @@ import { Context, Layer } from 'effect'
 import * as schema from './schema'
 
 const makeOrmClient = (url: string) => {
+	console.log('db url: %s', url)
 	const sqlite = new Database(url)
 	const ormClient = drizzle(sqlite, {
 		schema,
 		casing: 'snake_case',
 	})
 	migrate(ormClient, { migrationsFolder: 'drizzle' })
+	process.on('exit', () => sqlite.close())
+	process.on('SIGHUP', () => process.exit(128 + 1))
+	process.on('SIGINT', () => process.exit(128 + 2))
+	process.on('SIGTERM', () => process.exit(128 + 15))
 	return ormClient
 }
 
